2af9c60ca585d288bec94f8765a85ce781de1855
loki47 <larsentuta49@gmail.com>
2023-06-10 17:16:03 +0300

    Нет решения

---
+++ task003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Console.Write("Введите число: ");
+//int num = int.Parse(Console.ReadLine()!);
+//if (num == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// if (num == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+
+
+//string days = 
