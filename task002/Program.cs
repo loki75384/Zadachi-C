9b8d386b14d6c57bcc9349207839618a45c603f7
loki47 <larsentuta49@gmail.com>
2023-06-10 03:37:44 +0300

    Task complete

---
+++ task002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите 2 числа ");
+
+int number1 = int.Parse(Console.ReadLine()!);
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number1 == number2 * number2)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
