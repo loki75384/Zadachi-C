22420df8584d5ca30b3b77d9de8885c38af8a3a8
loki47 <larsentuta49@gmail.com>
2023-06-11 10:49:45 +0300

    task003/ complete

--- task003/Program.cs
+++ task003/Program.cs
@@ -1,13 +1,35 @@
-﻿//Console.Write("Введите число: ");
-//int num = int.Parse(Console.ReadLine()!);
-//if (num == 3)
-// {
-//     Console.WriteLine("Среда");
-// }
-// if (num == 5)
-// {
-//     Console.WriteLine("Пятница");
-// }
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+if (num == 3)
+{
+     Console.WriteLine("Среда");
+}
+if (num == 5)
+{
+    Console.WriteLine("Пятница");
+}
 
+if (num == 1)
+{
+    Console.WriteLine("Понедельник");
+}    
 
-//string days = 
+if (num == 2)
+{
+    Console.WriteLine(" Вторник");
+}
+
+if (num == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (num == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (num == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
